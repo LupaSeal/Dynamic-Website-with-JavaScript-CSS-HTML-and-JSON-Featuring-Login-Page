e3127cbafb6469296cc4bdfacd1a937de7701122
LupaSeal <110409402+LupaSeal@users.noreply.github.com>
2023-05-24 11:37:44 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require ("mongoose");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb+srv://demo:Mamamiapizzapia3!!!@cluster0.cpk97.mongodb.net/notesDB")
+
+//create a data schema
+const notesSchema = {
+    title: String,
+    content: String
+}
+
+const Note = mongoose.model('Note', notesSchema);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', function(req, res) {
+    let newNote = new Note({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newNote.save();
+    res.redirect('/');
+})
+
+app.listen(3000, function() {
+    console.log("server running");
+})
